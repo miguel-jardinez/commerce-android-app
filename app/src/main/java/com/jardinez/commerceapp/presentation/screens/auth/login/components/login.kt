27642a02d64991d0ae32a0ad8022ba20333bb189
miguel_jardinez <jardinez.ramos.miguel@gmail.com>
2023-05-21 14:28:01 -0600

    feat: add login navigation events

---
+++ app/src/main/java/com/jardinez/commerceapp/presentation/screens/auth/login/components/login.kt
@@ -0,0 +1,28 @@
+package com.jardinez.commerceapp.presentation.screens.auth.login.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.jardinez.commerceapp.presentation.navigation.Graph
+import com.jardinez.commerceapp.presentation.screens.auth.login.events.UIEvents
+import com.jardinez.commerceapp.presentation.screens.auth.login.view_model.LoginViewModel
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+fun Login(
+  navController: NavHostController,
+  viewModel: LoginViewModel = hiltViewModel()
+) {
+  LaunchedEffect(Unit) {
+    viewModel.uiEvents.collectLatest { event ->
+      when(event) {
+        UIEvents.NavigateToLogin -> {
+          navController.navigate(route = Graph.HOME) {
+            popUpTo(Graph.AUTH) { inclusive = true }
+          }
+        }
+      }
+    }
+  }
+}
