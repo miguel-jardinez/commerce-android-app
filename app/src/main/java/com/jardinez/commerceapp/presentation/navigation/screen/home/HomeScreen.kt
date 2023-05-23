92612858aad6ad3351f9294c493c12b3398ba39c
miguel_jardinez <jardinez.ramos.miguel@gmail.com>
2023-05-23 17:49:00 -0600

    feat: update java version 17

--- app/src/main/java/com/jardinez/commerceapp/presentation/navigation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/jardinez/commerceapp/presentation/navigation/screen/home/HomeScreen.kt
@@ -1,10 +1,10 @@
 package com.jardinez.commerceapp.presentation.navigation.screen.home
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class HomeScreen(
@@ -12,8 +12,8 @@ sealed class HomeScreen(
   val title: String,
   val icon: ImageVector
 ) {
-  object Home : HomeScreen("/home/products", "Home", Icons.Default.Home)
-  object Profile : HomeScreen("/home/profile", "Profile", Icons.Default.Person)
-  object Cart : HomeScreen("/home/cart", "Cart", Icons.Default.ShoppingCart)
-  object Settings : HomeScreen("/home/settings", "Config", Icons.Default.Settings)
+  object Home : HomeScreen("/home/products", "Home", Icons.Outlined.Home)
+  object Profile : HomeScreen("/home/profile", "Profile", Icons.Outlined.Person)
+  object Cart : HomeScreen("/home/cart", "Cart", Icons.Outlined.ShoppingCart)
+  object Settings : HomeScreen("/home/settings", "Config", Icons.Outlined.Menu)
 }
